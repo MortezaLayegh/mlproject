531f0effefe24d9d8a25fcf334b42e16ed8af00d
MortezaLayegh <Morteza.Layegh-Mirhosseini@city.ac.uk>
2023-10-27 18:31:42 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='MortezaLayegh',
+author='MortezaML',
 author_email='layeghmorteza@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
